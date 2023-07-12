6e695e106c569a9a2dbba4d57909533933ffee21
brewchetta <chett.tiller@gmail.com>
2023-07-12 16:00:44 -0400

    dom events with submit

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+// GLOBAL CONSTANTS //
+
 const exitButton = document.querySelector(".exit-button")
 const messageWindow = document.querySelector("#message-window")
 const fileSpan = document.querySelector("#file")
 const profileButton = document.querySelector(".file-options span:nth-child(2)")
 const helpButton = document.querySelector("#help")
+const messages = document.querySelector(".messages")
+const messageForm = document.querySelector(".message-form")
 
-function handleClickButton() {
+// CALLBACK FUNCTIONS //
+
+function handleClickExitButton() {
   messageWindow.remove()
 }
 
-exitButton.addEventListener("click", handleClickButton)
+// EVENT HANDLERS //
 
-// exitButton.onclick = () => messageWindow.remove()
-// exitButton.addEventListener("click", handleClickButton)
-// exitButton.onmouseover = handleClickButton
-// console.log(exitButton)
+exitButton.addEventListener("click", handleClickExitButton)
 
 fileSpan.addEventListener("click", function(event) {
   console.log("Hello world")
@@ -24,11 +27,38 @@ profileButton.addEventListener("mouseover", () => {
   console.log("Welcome current user blah blah blah blah")
 })
 
-function createMessage(input="Hello I am Clippy!") {
-  const newMessage = document.createElement("p")
-  const messageText = document.querySelector(".messages")
-  newMessage.innerText = input
-  messageText.append(newMessage)
-}
+helpButton.addEventListener("click", (event) => {
+  const newPTag = document.createElement("p")
+  newPTag.innerHTML = '<span class="blue-text">2:08pm</span> Hi I\'m Clippy!'
+  messages.append(newPTag)
+  console.log(event.target)
+})
 
-helpButton.addEventListener("click", createMessage)
+messageForm.addEventListener("submit", (event) => {
+  event.preventDefault()
+  // console.log(event.target.messageInput.value)
+  const messageForm = event.target
+  const messageInput = messageForm.messageInput
+  const messageValue = messageInput.value
+  // console.log(messageValue)
+  const myFancyNewParagraphTag = document.createElement("p")
+
+  const currentDate = new Date()
+  const minutes = currentDate.getMinutes()
+  const hours = currentDate.getHours()
+
+  myFancyNewParagraphTag.innerText = messageValue
+
+  const timeSpan = document.createElement("span")
+  timeSpan.textContent = `${hours}:${minutes}`
+  timeSpan.className = "blue-text"
+
+  myFancyNewParagraphTag.prepend(timeSpan)
+
+  const messages = document.querySelector(".messages")
+  messages.append(myFancyNewParagraphTag)
+})
+
+// here is an alternate way of attaching event listeners 
+// exitButton.onclick = () => messageWindow.remove()
+// exitButton.onmouseover = handleClickExitButton
